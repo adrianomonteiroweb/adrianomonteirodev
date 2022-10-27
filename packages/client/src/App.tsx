fdbbe0f3391ddb553812be93562ead9109d8ec73
adriano <adrianomonteiroweb@gmail.com>
2022-10-27 15:37:38 -0300

    feat: themes light & dark with styled-components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './styled-components/themes';
+
 import './App.css';
+import { GlobalStyles } from './styled-components/GlobalStyles';
+import { Container, Content } from './styled-components/styledComponents';
 
 function App() {
+  const [theme, setTheme] = useState('Light');
+
+  function changeTheme() {
+    theme === 'Light' ? setTheme('Dark') : setTheme('Light');
+  }
+
   return (
-    <div className='App'>
-      <h1>OK</h1>
-    </div>
+    <ThemeProvider theme={theme === 'Light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Container>
+        <Content className='content'>
+          <div className='App'>
+            <h1>OK</h1>
+            <button type='button' onClick={() => changeTheme()}>
+              theme
+            </button>
+          </div>
+        </Content>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ packages/client/src/styled-components/themes.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  body: '#1a1a1a',
+  fontColor: '#000000',
+  bar: '#d9d9d9',
+};
+
+export const darkTheme = {
+  body: '#e9ebf2',
+  fontColor: '#ffffff',
+  bar: '#0e0e0e',
+};
