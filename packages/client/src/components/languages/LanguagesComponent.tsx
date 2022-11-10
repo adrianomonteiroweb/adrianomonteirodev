6afc7c901121632878ddb3173fa8f696e7986e64
adriano <adrianomonteiroweb@gmail.com>
2022-11-10 16:16:38 -0300

    feat: translate method with i18next and react-i18next

--- packages/client/src/components/languages/LanguagesComponent.tsx
+++ packages/client/src/components/languages/LanguagesComponent.tsx
@@ -1,11 +1,28 @@
+import i18n from '../../i18next';
+
 import './languagesComponent.css';
 
 export default function LanguagesComponent() {
   return (
     <div className='languages-div'>
-      <button className='language-button selected'>BR</button>
-      <button className='language-button'>ENG</button>
-      <button className='language-button'>ESP</button>
+      <button
+        className='language-button selected'
+        onClick={() => i18n.changeLanguage('pt-BR')}
+      >
+        BR
+      </button>
+      <button
+        className='language-button'
+        onClick={() => i18n.changeLanguage('en-US')}
+      >
+        ENG
+      </button>
+      <button
+        className='language-button'
+        onClick={() => i18n.changeLanguage('es-ES')}
+      >
+        ESP
+      </button>
     </div>
   );
 }
