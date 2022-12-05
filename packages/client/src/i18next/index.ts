7349e8091c62fb59e5b01ac02795238a71b3d9e3
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-12-04 22:20:10 -0300

    feat: implement french language

--- packages/client/src/i18next/index.ts
+++ packages/client/src/i18next/index.ts
@@ -4,11 +4,13 @@ import { initReactI18next } from 'react-i18next';
 import PTBR from './locales/pt-br/pt-br.json';
 import ENUS from './locales/en-us/en-us.json';
 import ESES from './locales/es-es/es-es.json';
+import FRFR from './locales/fr-fr/fr-fr.json';
 
 const resources = {
   'pt-BR': PTBR,
   'en-US': ENUS,
   'es-ES': ESES,
+  'fr-FR': FRFR,
 };
 
 function InitI18N() {
