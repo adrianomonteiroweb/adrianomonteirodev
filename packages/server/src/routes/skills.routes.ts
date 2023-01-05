390b0c89457f1eaae3397fb642398f67ffafdcdd
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-01-05 14:36:04 -0300

    feat: implemented cors for client and server comunication

--- packages/server/src/routes/skills.routes.ts
+++ packages/server/src/routes/skills.routes.ts
@@ -3,6 +3,8 @@ import { allSkills } from '../data/skillsData';
 
 const skillsRouter = express.Router();
 
-skillsRouter.get('/skills', (_req, res) => res.status(200).json(allSkills));
+skillsRouter.get('/skills', (_req, res) =>
+  res.header('Access-Control-Allow-Origin', '*').status(200).json(allSkills)
+);
 
 export default skillsRouter;
