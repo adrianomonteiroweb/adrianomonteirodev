41e916e76a4d501727f530b8f10f8ed9fee176be
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2023-01-07 14:20:28 -0300

    feat: implemented hover style

--- packages/client/src/components/menu/MenuComponent.tsx
+++ packages/client/src/components/menu/MenuComponent.tsx
@@ -7,7 +7,31 @@ interface IProps {
   theme: any;
 }
 
+function hover(element: any, theme: string) {
+  element.setAttribute(
+    'src',
+    theme === 'Light' ? 'light-theme-color.png' : 'dark-theme-color.png'
+  );
+}
+
+function unhover(element: any, theme: string) {
+  element.setAttribute(
+    'src',
+    theme === 'Light' ? 'light-theme.png' : 'dark-theme.png'
+  );
+}
+
 export default function MenuComponent({ changeTheme, theme }: IProps) {
+  const themeIcon = document.querySelector('#theme-icon');
+
+  themeIcon?.addEventListener('mouseover', () => {
+    hover(themeIcon, theme);
+  });
+
+  themeIcon?.addEventListener('mouseout', () => {
+    unhover(themeIcon, theme);
+  });
+
   return (
     <div className='menu'>
       <LanguagesComponent />
@@ -17,6 +41,7 @@ export default function MenuComponent({ changeTheme, theme }: IProps) {
         onClick={() => changeTheme()}
       >
         <img
+          id='theme-icon'
           src={theme === 'Light' ? 'light-theme.png' : 'dark-theme.png'}
           alt={theme === 'Light' ? 'light-theme' : 'dark-theme'}
         />
